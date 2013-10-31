d524594753f531d206691008078de979c4128329
acarvalh <alexandracaan@gmail.com>
2013-10-31 17:03:00 +0100

    Create models_mgg_bkg.rs

---
+++ models_mgg_bkg.rs
@@ -0,0 +1,16 @@
+mgg[100,180];
+
+mgg_bkg_8TeV_slope_cat0[1.0,0, 1];
+mgg_bkg_8TeV_slope1_cat0[0.9,-10.0, 10.0];
+mgg_bkg_8TeV_slope2_cat0[0.6,-10.0, 10.0];
+mgg_bkg_8TeV_slope3_cat0[0.9,-10.0, 10.0];
+
+mgg_bkg_8TeV_slope_cat1[1.0,0, 1];
+mgg_bkg_8TeV_slope1_cat1[0.9,-10.0, 10.0];
+mgg_bkg_8TeV_slope2_cat1[0.6,-10.0, 10.0];
+mgg_bkg_8TeV_slope3_cat1[0.3,-10.0, 10.0];
+
+mgg_bkg_8TeV_slope_cat2[1.0,0, 1];
+mgg_bkg_8TeV_slope1_cat2[0.9,-10.0, 10.0];
+mgg_bkg_8TeV_slope2_cat2[0.6,-10.0, 10.0];
+mgg_bkg_8TeV_slope3_cat2[0.3,-10.0, 10.0];
