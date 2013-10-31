ea22060969384673a1c55c9d5a5137d5e0342b95
acarvalh <alexandracaan@gmail.com>
2013-10-31 22:39:15 +0100

    only the parameters that matters

--- moddels_mtot_bkg.rs
+++ moddels_mtot_bkg.rs
@@ -1,10 +1,8 @@
 mtot[320,1200];
-CMS_mtot_bkg_8TeV_slope_cat0[10.0,-500, 500];
-CMS_mtot_bkg_8TeV_slope1_cat0[10., -500, 500.0];
-CMS_mtot_bkg_8TeV_slope2_cat0[0.,0.0, 0.0];
-CMS_mtot_bkg_8TeV_cat0_norm[0.,0.0, 1000.0];
 
-CMS_mtot_bkg_8TeV_slope_cat1[10.0,-500, 500.0];
-CMS_mtot_bkg_8TeV_slope1_cat1[1., -500, 500.0];
-CMS_mtot_bkg_8TeV_slope2_cat1[0.0,0.0, 0.0];
-CMS_mtot_bkg_8TeV_cat1_norm[0.,0.0, 1000.0];
+CMS_mtot_bkg_8TeV_slope1_cat0[10., -1000, 1000.0];
+CMS_mtot_bkg_8TeV_slope2_cat0[10., -1000, 1000.0];
+
+
+CMS_mtot_bkg_8TeV_slope1_cat1[10., -1000, 1000.0];
+CMS_mtot_bkg_8TeV_slope2_cat1[10., -1000, 1000.0];
